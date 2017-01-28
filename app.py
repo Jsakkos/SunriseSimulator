2cd530954b6893b208ed8344b8f6c7922075cf5d
Jsakkos <jonathansakkos@gmail.com>
2017-01-28 17:52:34 -0600

    Added button event handler

--- app.py
+++ app.py
@@ -1,55 +1,40 @@
 # Sunrise simulator##
 # Adapted from https://github.com/rasathus/circadianLighting##
 
-import logging
 import os
 import random
 import sys
 import threading
 import time
-from datetime import datetime
-from logging.handlers import RotatingFileHandler
+from datetime import datetime, date
 from queue import Queue
 
 import pigpio
 from flask import Flask, render_template, jsonify
 
-# Setup Logger
-log_formatter = logging.Formatter('%(asctime)s %(threadName)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s',
-                                  datefmt='%m/%d/%Y %I:%M:%S %p')
-logFile = 'log.txt'
-my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=5 * 1024 * 1024, backupCount=2, encoding=None, delay=1)
-my_handler.setFormatter(log_formatter)
-my_handler.setLevel(logging.DEBUG)
-app_log = logging.getLogger('root')
-app_log.setLevel(logging.DEBUG)
-app_log.addHandler(my_handler)
-
 # Start PIGPIO to control GPIO pins
-app_log.debug('Starting PIGPIO')
 pi = pigpio.pi()
 if not pi.connected:
     exit()
-    app_log.warning("Can't connect to Pi")
 
 
 # Time modes
 class auto_settings:
-    def __init__(self, hour, minute, startRGB, finishRGB, duration):
+    def __init__(self, hour, minute, color, duration):
         self.hour = hour
         self.minute = minute
-        self.startRGB = startRGB
-        self.finishRGB = finishRGB
+        self.color = color
         self.duration = duration
 
 
 # Auto settings
-Wakeup = auto_settings(7, 30, [0, 0, 0], [255, 109, 0], 5000)
-Wakeup2 = auto_settings(7, 45, [255, 109, 0], [255, 255, 255], 5000)
-Bedtime = auto_settings(22, 30, [255, 100, 0], [0, 0, 0], 1000)
+Wakeup = auto_settings(7, 30, [255, 109, 0], 500)
+Wakeup2 = auto_settings(7, 45, [255, 255, 255], 500)
+Bedtime = auto_settings(22, 30, [255, 100, 0], 100)
 
 
 # Color conversions
+
 def hex_to_rgb(value):
     value = value.lstrip('#')
     lv = len(value)
@@ -63,50 +48,39 @@ def rgb_to_hex(rgb):
 # LED controller class
 class LED_Communicator:
     def __init__(self):
-        self.auto_resume_job = None
         self.queue = Queue()
         self.mode = 'auto'
         self.state = [0, 0, 0]
         self.set = [0, 0, 0]
         self.set_mood = [0, 0, 0]
         self.run = True
-        self.clear = False
-        app_log.debug("Communicator starting main_loop")
-        self.thread = threading.Thread(name='Communicator', target=self.main_loop)
-        self.thread.start()
-        app_log.debug("Mode loop thread starting")
-        self.mode_thread = threading.Thread(name='Mode Loop', target=self.mode_loop)
-        self.mode_thread.start()
         self.button_event = threading.Event()
         self.button_event.clear()
         self.lock = threading.Lock()
-        app_log.debug("Communicator init complete.")
+        self.delay = 20
+
         # set GPIO pins being used to control LEDs
         self.pins = [17, 22, 24]
-        app_log.info("Running resume auto, in case were in an auto event.")
-        self.resume_auto()
-        app_log.debug("Communicator init complete.")
+        self.thread = threading.Thread(name='Communicator', target=self.main_loop)
+        self.thread.start()
+        self.mode_thread = threading.Thread(name='Mode Loop', target=self.mode_loop)
+        self.mode_thread.start()
+        self.write(self.set)
 
     def get_state(self):
-        app_log.info('Desired settings: {} set to {}'.format(self.pins, self.set))
         # turned off for testing
         for i in range(3):
             self.state[i] = pi.get_PWM_dutycycle(self.pins[i])
-        app_log.info('Pins {} set to {}'.format(self.pins, self.state))
+            # app_log.info('Pins {} set to {}'.format(self.pins, self.state))
 
     def write(self, set_state):
-
         for i in range(3):
             # turned off for testing
             pi.set_PWM_dutycycle(self.pins[i], set_state[i])
-            # print(self.pins,set_state)
         self.get_state()
-        # self.state = set_state
-        app_log.debug('LED state {}'.format(self.state))
 
     def main_loop(self):
         try:
-            app_log.debug("main_loop - processing queue ...")
             while self.run:
                 # get desired LED color from the queue
                 lighting_event = self.queue.get(block=True)
@@ -115,99 +89,91 @@ class LED_Communicator:
 
         except KeyboardInterrupt:
             self.run = False
-            app_log.warning("Caught keyboard interrupt in main_loop.  Shutting down ...")
 
-    def transition(self, set_state, transition_duration=100, transition_mode='fade'):
+    def transition(self, set_state, transition_duration=100):
         self.set = set_state
         # clear queue
         self.clear_queue()
-        app_log.info("Current state is : %s , destination state is : %s , transitioning via %s in : %d ticks" % (
-            self.state, self.set, transition_mode, transition_duration))
-        if transition_mode is 'fade':
-            for transition_count in range(transition_duration - 1):
-                RGB = []
-                self.get_state()
-                for component in range(3):
-                    RGB.append(int((self.state[component] + (
-                        self.set[component] - self.state[component]) * transition_count / transition_duration)))
+        for transition_count in range(transition_duration - 1):
+            RGB = []
+            self.get_state()
+            for component in range(3):
+                RGB.append(int((self.state[component] + (
+                    self.set[component] - self.state[component]) * transition_count / transition_duration)))
+            if RGB != self.state:
                 self.queue.put(RGB)
-                time.sleep(.01)
-                # self.queue.put(self.set)
-
-    def clear_mode(self):
-        app_log.debug("Removing mode")
-        with self.lock:
-            self.mode = []
+            time.sleep(self.delay)
 
     def clear_queue(self):
         with self.queue.mutex:
             self.queue.queue.clear()
 
-    def resume_auto(self):
-        self.clear_mode()
-        self.mode = 'auto'
+    def change_mode(self, mode):
+        self.mode = mode
+        self.button_event.set()
         self.clear_queue()
-        self.write([0, 0, 0])
-        app_log.debug("Resume auto called, system state is now : {}".format(self.mode))
+        if mode is 'auto':
+            self.transition([0, 0, 0])
+            self.delay = 20
+        elif mode is 'cycle':
+            self.delay = .2
+        elif mode is 'mood':
+            self.delay = .2
+        else:
+            self.delay = 0.01
 
-    # todo split mode loop into event loop and mood/cycle loop
     def mode_loop(self):
 
         try:
-            app_log.debug('Starting mode loop')
-
             while self.run:
+                print('Starting Mode loop')
                 # Auto mode loop
-                # todo make transition duration accurate
                 # todo add scheduling from google calendar
                 self.button_event.clear()
-                if self.mode == 'auto':
-                    app_log.info('{} mode running'.format(self.mode))
+                if self.mode is 'auto':
                     # Get the current time
                     now = datetime.now().time()
-                    #  morning fade in
-                    if now.hour == Wakeup.hour and now.minute == Wakeup.minute and self.mode == 'auto':
-
-                        self.transition(Wakeup.finishRGB, Wakeup.duration)
-                        # alert fade out
-                    elif now.hour == Wakeup2.hour and now.minute == Wakeup2.minute and self.mode == 'auto':
-                        self.transition(Wakeup2.finishRGB, Wakeup2.duration)
-                        time.sleep(30)
+                    today = date.weekday(date.today())
+                    weekend = False
+                    if today is 5 or today is 6:
+                        weekend = True
+                    else:
+                        weekend = False
+                    # morning fade in
+                    if now.hour is Wakeup.hour and now.minute is Wakeup.minute and weekend is False and self.mode is 'auto':
+                        self.transition(Wakeup.color, Wakeup.duration)
+                        self.button_event.wait(timeout=30)
+                        self.transition(Wakeup2.color, Wakeup2.duration)
+                        self.button_event.wait(timeout=30)
                         self.transition([0, 0, 0])
-                    elif now.hour == Bedtime.hour and now.minute == Bedtime.minute and self.mode == 'auto':
-                        self.transition(Bedtime.startRGB)
-                        time.sleep(600)
-                        self.transition(Bedtime.finishRGB, Bedtime.duration)
-                    # time.sleep(30)
-                    while not self.button_event.wait(timeout=30):
-                        pass
-                    self.button_event.clear()
-                elif self.mode == 'mood':
-                    app_log.info('{} mode running'.format(self.mode))
-                    app_log.debug('{} mode enabled'.format(self.mode))
-                    self.transition(self.set_mood, 500)
-                    # time.sleep(3)
-                    while not self.button_event.wait(timeout=3):
-                        pass
-                    self.button_event.clear()
-                    self.transition([0, 0, 0], 500)
-                    #todo slow down transitions
+                    elif now.hour is Bedtime.hour and now.minute is Bedtime.minute and weekend is False and self.mode is 'auto':
+                        self.transition(Bedtime.color)
+                        self.button_event.wait(timeout=600)
+                        self.transition(Bedtime.color, Bedtime.duration)
+                        self.button_event.wait(timeout=30)
+
+                    self.button_event.wait(timeout=30)
+                elif self.mode is 'lamp':
+                    self.transition(self.set, 50)
+                elif self.mode is 'mood':
+                    self.transition(self.set_mood, 200)
+                    self.button_event.wait(timeout=3)
+                    #
+                    self.transition([0, 0, 0], 200)
+
                 elif self.mode == 'cycle':
-                    app_log.info('{} mode running'.format(self.mode))
                     color = []
                     for i in range(3):
                         color.append(random.randint(0, 255))
-                    self.transition(color, 500)
-                    # time.sleep(1)
-                    while not self.button_event.wait(timeout=1):
-                        pass
-                    self.button_event.clear()
+                    self.transition(color, 200)
+                    self.button_event.wait(timeout=2)
+                self.button_event.wait(timeout=.2)
+                self.button_event.clear()
+
         except KeyboardInterrupt:
             self.run = False
-            app_log.warning("Caught keyboard interrupt in mode_loop.  Shutting down ...")
 
     def shutdown(self):
-
         # send final state to avoid blocking on queue.
         self.queue.put([0, 0, 0])
         self.run = False
@@ -233,74 +199,55 @@ def index():
 # sends the current RGB values to flask
 @app.route('/get/current_state')
 def get_state():
-    app_log.info('Get state {}'.format(LED.state))
     return jsonify({'state': "%s" % rgb_to_hex(tuple(LED.state))})
 
 
 @app.route('/get/current_mode')
 def get_mode():
-    app_log.info('Mode is {}'.format(LED.mode))
     return jsonify({'mode': "%s" % LED.mode})
 
 
 @app.route('/mode/off')
 def off_mode():
-    app_log.debug('Initiate shutdown')
     LED.shutdown()
     return jsonify({'success': True})
 
 
 @app.route('/mode/auto')
 def auto_mode():
-    app_log.info('Auto mode called')
-    LED.button_event.set()
-    LED.mode = 'auto'
-    app_log.info('Auto mode set')
-    get_state()
-    app_log.info('Calling Resume Auto')
-    LED.resume_auto()
-    app_log.info('{} mode enabled'.format(LED.mode))
+    if LED.mode is not 'auto':
+        LED.change_mode('auto')
     return jsonify({'success': True})
 
 
 @app.route('/mode/lamp/<hex_val>', methods=['GET', 'POST'])
 def lamp_mode(hex_val):
-    LED.clear_mode()
-    LED.button_event.set()
-    LED.mode = 'lamp'
-    app_log.info('{} mode enabled'.format(LED.mode))
-    LED.clear_queue()
-    app_log.info('Desired color is {}'.format(hex_to_rgb(hex_val)))
-    LED.transition(hex_to_rgb(hex_val))
+    if LED.mode is not 'lamp':
+        LED.change_mode('lamp')
+    LED.set = hex_to_rgb(hex_val)
     return jsonify({'success': True})
 
 
 @app.route('/mode/mood/<hex_val>', methods=['GET', 'POST'])
 def mood_mode(hex_val):
-    LED.button_event.set()
-    LED.mode = 'mood'
-    app_log.info('{} mode enabled'.format(LED.mode))
-    get_mode()
-    app_log.info('Desired color is {}'.format(hex_to_rgb(hex_val)))
-    LED.set = hex_to_rgb(hex_val)
+    if LED.mode is not 'mood':
+        LED.change_mode('mood')
     LED.set_mood = hex_to_rgb(hex_val)
     return jsonify({'success': True})
 
 
 @app.route('/mode/cycle')
 def cycle_mode():
-    LED.button_event.set()
-    LED.mode = 'cycle'
-    app_log.info('{} mode enabled'.format(LED.mode))
-    get_mode()
+    if LED.mode is not 'cycle':
+        LED.change_mode('cycle')
     return jsonify({'success': True})
 
 
+# set the secret key.
+app.secret_key = os.urandom(24)
+
 if __name__ == '__main__':
-    # initialize LED class
     LED = LED_Communicator()
     app.run(host='0.0.0.0')
-    app_log.info("Calling shutdown on led chain")
     LED.shutdown()
     exit(0)
-# end flask
